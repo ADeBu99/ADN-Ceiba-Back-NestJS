078c7025c6b049a90944c0b8bde9ea884e85e1a4
ADeBu99 <adrian.asuncion@ceiba.com.co>
2021-07-12 16:00:47 -0500

    Ajusting joi package

--- src/infraestructura/infraestructura.module.ts
+++ src/infraestructura/infraestructura.module.ts
@@ -1,4 +1,4 @@
-import * as Joi from '@hapi/joi';
+import * as Joi from 'joi';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppLogger } from './configuracion/ceiba-logger.service';
